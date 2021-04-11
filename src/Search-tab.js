c75c6540f0c66417d22a60195ee251cb57f6aa9c
mafaldamac <mafaldamac@hotmail.com>
2021-04-11 17:27:28 +0100

    insert form & button

--- src/Search-tab.js
+++ src/Search-tab.js
@@ -2,5 +2,21 @@ import React from "react";
 import "./Search-tab.css";
 
 export default function Search() {
-  return <div>Search Engine here</div>;
+  return (
+    <div>
+      Search Engine here
+      <div>
+        <form>
+          <input
+            type="search"
+            placeholder="Write a city here"
+            className="form-search"
+          />
+          <button type="submit" class="btn btn-outline-warning">
+            Search
+          </button>
+        </form>
+      </div>
+    </div>
+  );
 }
