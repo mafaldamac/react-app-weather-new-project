164b3ee49d3a4bd47f8f91d975816755487e01c2
mafaldamac <mafaldamac@hotmail.com>
2021-04-11 18:45:03 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,14 +1,12 @@
-import React from "react";
+import { StrictMode } from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
+
 import App from "./App";
-import reportWebVitals from "./reportWebVitals";
 
+const rootElement = document.getElementById("root");
 ReactDOM.render(
-  <React.StrictMode>
+  <StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById("root")
+  </StrictMode>,
+  rootElement
 );
-
-reportWebVitals();
