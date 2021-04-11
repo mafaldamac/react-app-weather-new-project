c06f52eb133338333135c9ffc1dd4d451282d734
mafaldamac <mafaldamac@hotmail.com>
2021-04-11 16:49:23 +0100

    add bootstrap & starting html

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
-import logo from "./logo.svg";
 import "./App.css";
+import "bootstrap/dist/css/bootstrap.css";
+import Weather from "./Weather";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <div className="container">
+        <h1>Weather App</h1>
+        <div>
+          <Weather />
+        </div>
+        <footer>
+          This project was coded by Mafalda Lourenço and is
+          <a href="https://github.com/mafaldamac/react-app-weather-new-project/">
+            {" "}
+            open-source on Github{" "}
+          </a>
+        </footer>
+      </div>
+    </div>
+  );
 }
-
-App;
